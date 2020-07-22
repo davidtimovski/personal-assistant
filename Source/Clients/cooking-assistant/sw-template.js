14c05c9f4b5161a2b87e8e15e374bb75071d05ab
David Timovski <davidtimovski@protonmail.com>
2020-07-22 13:42:30 +0200

    Up version in Cooking Assistant service worker

--- Source/Clients/cooking-assistant/sw-template.js
+++ Source/Clients/cooking-assistant/sw-template.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "1.9.11";
+const CACHE_NAME = "1.9.12";
 const notificationIconUri = "/images/icons/app-icon-96x96.png";
 const defaultProfileImageUri =
   "https://res.cloudinary.com/personalassistant/t_profile_thumbnail/production/defaults/sfmqac.jpg";
