45f499ff42e9be4fc331053c6e314b239423ef7e
David Timovski <davidtimovski@protonmail.com>
2022-10-07 17:07:02 +0200

    Rework Worker to be invoked by cron. Rename to Jobs.

---
+++ src/Jobs/Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Application.Contracts.Common;
+using CloudinaryDotNet;
+using Infrastructure.Cdn;
+using Jobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostContext, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("appsettings.json", false);
+        config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true, true);
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureLogging((hostContext, logging) =>
+    {
+        if (hostContext.HostingEnvironment.EnvironmentName == Environments.Production)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(hostContext.Configuration)
+                .CreateLogger();
+
+            logging.AddSerilog(Log.Logger, dispose: true);
+        }
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddSingleton<ICdnService>(new CloudinaryService(
+            cloudinaryAccount: new Account(
+                hostContext.Configuration["Cloudinary:CloudName"],
+                hostContext.Configuration["Cloudinary:ApiKey"],
+                hostContext.Configuration["Cloudinary:ApiSecret"]),
+            hostContext.HostingEnvironment.EnvironmentName,
+            hostContext.Configuration["DefaultImageUris:Profile"],
+            hostContext.Configuration["DefaultImageUris:Recipe"],
+            new HttpClient()));
+
+        services.AddHttpClient("fixer", c =>
+        {
+            c.BaseAddress = new Uri("http://data.fixer.io/api/");
+        });
+
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+        services.AddTransient<MidnightJob>();
+    })
+    .Build();
+
+static async Task RunWorkerAsync(IServiceProvider services)
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    var job = serviceScope.ServiceProvider.GetRequiredService<MidnightJob>();
+    await job.RunAsync();
+}
+
+await host.StartAsync();
+await RunWorkerAsync(host.Services);
+await host.StopAsync();
