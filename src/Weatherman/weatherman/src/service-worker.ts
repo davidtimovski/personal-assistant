bd26ccd54b2def875ad0e60f2443c921ac4742f9
David Timovski <davidtimovski@protonmail.com>
2026-01-11 20:51:47 +0100

    Bump Weatherman version

--- src/Weatherman/weatherman/src/service-worker.ts
+++ src/Weatherman/weatherman/src/service-worker.ts
@@ -1,6 +1,6 @@
 import { build, files } from '$service-worker';
 
-const APP_VERSION = '1.0.5';
+const APP_VERSION = '1.0.6';
 
 self.addEventListener('install', (event: ExtendableEvent) => {
 	event.waitUntil(
