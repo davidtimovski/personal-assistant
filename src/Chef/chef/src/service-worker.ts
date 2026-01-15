95405e6c7aee59dea63a861e48812f095e871931
David Timovski <davidtimovski@protonmail.com>
2026-01-15 20:29:09 +0100

    Bump Chef version

--- src/Chef/chef/src/service-worker.ts
+++ src/Chef/chef/src/service-worker.ts
@@ -1,6 +1,6 @@
 import { build, files } from '$service-worker';
 
-const APP_VERSION = '2.0.0-beta.10';
+const APP_VERSION = '2.0.0-beta.11';
 
 self.addEventListener('install', (event: ExtendableEvent) => {
 	event.waitUntil(
